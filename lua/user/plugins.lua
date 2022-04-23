a2c39fac52121789f8c48afd8b9d8cc4fa48d92d
Tyler Pugmire <tyler.pugmire@yahoo.com>
2022-04-23 14:08:04 -0700

    bufferline setup

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,6 +40,8 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim"
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
+  use "akinsho/bufferline.nvim"
+  use "moll/vim-bbye"
 
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
