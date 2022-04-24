d45a3eed7dc15fcb3323ded1eb47061c9666d7ae
Tyler Pugmire <tyler.pugmire@yahoo.com>
2022-04-24 14:25:10 -0700

    setup impatient plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "nvim-lualine/lualine.nvim"
   use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
+  use "lewis6991/impatient.nvim"
 
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
