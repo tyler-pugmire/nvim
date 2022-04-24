e45b7f4e3dae6263e268460ad1dd72ee6155f756
Tyler Pugmire <tyler.pugmire@yahoo.com>
2022-04-24 14:54:08 -0700

    setup alpha plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,6 +46,8 @@ return packer.startup(function(use)
   use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
+  use "goolord/alpha-nvim"
+  use "antoinemadec/FixCursorHold.nvim"
 
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
